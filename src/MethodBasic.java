5935c4d17ba35844fc503ed0b83bd9494788a2d2
srinivas <srinivasmyllapalli10@gmail.com>
2026-06-29 19:39:14 +0530

    Added method parameters

---
+++ src/MethodBasic.java
@@ -0,0 +1,11 @@
+class StudentBasic {
+    void greet() {
+        System.out.println("HELLO SRINIVAS");
+    }
+}
+public class MethodBasic {
+    public static void main(String[] args) {
+        StudentBasic s1 = new StudentBasic();
+        s1.greet();
+    }
+}
